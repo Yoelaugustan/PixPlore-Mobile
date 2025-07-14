f5cff32ce0c0ef6e3f81512577622b4a4dae9e7c
Yoel Augustan <yoelagustan@gmail.com>
2025-07-14 13:12:25 +0700

    Create ScreenWrapper Component

---
+++ components/ScreenWrapper.tsx
@@ -0,0 +1,33 @@
+import { Dimensions, Platform, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { ScreenWrapperProps } from '@/types'
+
+const {height} = Dimensions.get('window')
+
+const ScreenWrapper = ({style, children, isModal}: ScreenWrapperProps) => {
+  let paddingTop = Platform.OS == 'ios'? height * 0.06 : 40;
+  let paddingBottom = 0 
+
+  if(isModal){
+    let paddingTop = Platform.OS == 'ios'? height * 0.02 : 45;
+    let paddingBottom = height * 0.02 
+  }
+  return (
+    <View 
+        style={[
+            {
+                paddingTop,
+                paddingBottom,
+                flex: 1,
+            },
+            style
+    ]}>
+        <StatusBar barStyle='light-content' />
+        {children}
+    </View>
+  )
+}
+
+export default ScreenWrapper
+
+const styles = StyleSheet.create({})
---
+++ types.ts
@@ -0,0 +1,7 @@
+import { ViewStyle } from "react-native"
+
+export type ScreenWrapperProps = {
+    style?: ViewStyle;
+    children: React.ReactNode;
+    isModal?: boolean;
+}
