40e9aea47ce929df95e0200cecd6da2b68a13d1e
Yoel Augustan <yoelagustan@gmail.com>
2025-07-14 19:43:53 +0700

    Add types or Interface for Input, BackButton and CustomButton Component

--- types.ts
+++ types.ts
@@ -1,7 +1,26 @@
-import { ViewStyle } from "react-native"
+import { StyleProp, TextInput, TextInputProps, TextStyle, TouchableOpacityProps, ViewStyle } from "react-native"
 
 export type ScreenWrapperProps = {
     style?: ViewStyle;
     children: React.ReactNode;
     isModal?: boolean;
+}
+
+export interface InputProps extends TextInputProps {
+    icon?: React.ReactNode;
+    containerStyle?: ViewStyle;
+    inputStyle?: TextStyle;
+    inputRef?: React.RefObject<TextInput>;
+}
+
+export type BackButtonProps = {
+    style?: ViewStyle;
+    iconSize?: number;
+}
+
+export interface CustomButtonProps extends TouchableOpacityProps {
+    style?: StyleProp<ViewStyle>;
+    onPress?: () => void;
+    loading?: boolean;
+    children: React.ReactNode;
 }
