7afb95451640f48b904439a9bb47166ae9e26627
Yoel Augustan <yoelagustan@gmail.com>
2025-07-15 16:29:17 +0700

    Combine Auth Page with HomePage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,66 +1,15 @@
-import { Ionicons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
+import 'structured-clone'
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          backgroundColor: '#0f0D23',
-          borderRadius: 50,
-          marginHorizontal: 20,
-          marginBottom: 36,
-          height: 42,
-          position: 'absolute',
-          overflow: 'hidden',
-          borderWidth: 1,
-          borderColor: '#0f0D23',
-        }
-      }}
-    >
-      <Tabs.Screen 
-        name='index'
-        options={{
-          title: 'Home',
-          headerShown: false,
-          tabBarIcon: ({ color, size, focused }) => (
-            <Ionicons
-              name={focused ? 'home' : 'home-outline'}
-              size={size}
-              color={color}
-            />
-          ),
-        }}
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen 
+        name="index" 
+        options={{ 
+          title: 'index', 
+        }} 
       />
-      <Tabs.Screen 
-        name='cameraPage'
-        options={{
-          title: 'Pix it!',
-          headerShown: false,
-          tabBarIcon: ({ color, size, focused }) => (
-            <Ionicons
-              name={focused ? 'camera' : 'camera-outline'}
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen 
-        name='flashCardPage'
-        options={{
-          title: 'Flash Cards',
-          headerShown: false,
-          tabBarIcon: ({ color, size, focused }) => (
-            <Ionicons
-              name={focused ? 'book' : 'book-outline'}
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+    </Stack>
   )
 }
